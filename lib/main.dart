2f276512724ad8ebaee437da20e609bc3b79095f
YASSA MOURIS <146125680+yassaYM7@users.noreply.github.com>
2024-09-09 10:28:30 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'task_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TaskHomePage(),
+    );
+  }
+}
